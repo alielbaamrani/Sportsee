b257f190ac7cdbf6cdb3a284b0c80cd2a3ed9b4f
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-06-08 15:38:32 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import "./Header.scss"
+
+export default function Header() {
+    
+  return (
+    <div className="Head">
+      <img src={logo} alt="sportSee" aria-label="logo sportSee" />
+      <nav>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="#">Profil</NavLink>
+        <NavLink to="#">Réglages</NavLink>
+        <NavLink to="#">Communauté</NavLink>
+      </nav>
+    </div>
+  );
+};
+
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../utils/reset.scss";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+export default Home;
