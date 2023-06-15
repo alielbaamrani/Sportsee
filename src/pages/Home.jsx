29fe0cf1d4a58c77565a6a467ab381e999a9353f
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-06-15 15:45:14 +0200

    HomePageFix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { HomePage } from "../styles/HomePage";
 import "../styles/reset.scss";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <HomePage>
+      <NavLink to="user/12"> Karl </NavLink>
+      
+      <NavLink to="user/18"> Cecilia </NavLink>
+    </HomePage>
+  );
 }
 
 export default Home;
