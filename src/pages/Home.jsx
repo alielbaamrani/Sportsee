ec68b51cc3a854dccd8e3bf8caf118cefafa5f4a
Ali <ali.elbaamrani21@gmail.com>
2023-12-08 15:26:25 +0100

    build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { HomePage } from "../styles/HomePage";
@@ -6,9 +8,13 @@ import "../styles/reset.scss";
 function Home() {
   return (
     <HomePage>
-      <NavLink to="user/12"> Karl </NavLink>
-      
-      <NavLink to="user/18"> Cecilia </NavLink>
+      <NavLink className="link" to="user/12">
+        Karl &#x1F466;
+      </NavLink>
+
+      <NavLink className="link" to="user/18">
+        Cecilia &#x1F469;
+      </NavLink>
     </HomePage>
   );
 }
--- src/styles/HomePage.js
+++ src/styles/HomePage.js
@@ -1,10 +1,19 @@
+/** @format */
+
 import styled from "styled-components";
 
 const HomePage = styled.div`
+  width: 100vw;
+  height: 70vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-export { HomePage };
+const Navlink = styled.a`
+  background-color: #000;
+  color: #4f5;
+`;
+
+export { HomePage, Navlink };
